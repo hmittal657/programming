e701008dbe8fe83cec40aa15ad00b6f57409709d
Souvik Gayen <gayensouvik1@gmail.com>
2018-10-14 15:25:25 +0530

    Refactoring with variable names

--- 230b _ using sieve.cpp
+++ 230b _ using sieve.cpp
@@ -7,30 +7,30 @@ void sieve(){
 	a[0]=true;
 	a[1]=true;
 	for(int i=2;i<1001;i++){
-		if(a[i] == false){
+		if(a[i] == 0){
 			for(int j=i*i;j<1000001;j+=i){
-				a[j]=true;
+				a[j]=1;
 			}
 		}
 	}
 }
 
 bool isprime(int x){
-	if(x == 1) return false;
-	if(x == 2) return true;
-	if(x == 3) return true;
-	if(x%2 == 0) return false;
-	if(x%3 == 0) return false;
+	if(x == 1) return 0;
+	if(x == 2) return 1;
+	if(x == 3) return 1;
+	if(x%2 == 0) return 0;
+	if(x%3 == 0) return 0;
 	int div=5;
 	int diff=2;
 	while(div*div <= x){
 		if(x % div == 0){
-			return false;
+			return 0;
 		}
 		div += diff;
 		diff = 6-diff;
 	}
-	return true;
+	return 1;
 }
 
 int main(){
@@ -47,11 +47,9 @@ int main(){
 			continue;
 		}
 		double sq = sqrt(x);
-		long long jk = (long long)sq;
-		// if(a[jk] == false){
-		// 	printf("Prime\n");
-		// }
-		if(jk*jk == x && a[jk] == false){
+		long long index = (long long)sq;
+		
+		if(index*index == x && a[index] == 1){
 			printf("YES\n");
 		}else{
 			printf("NO\n");
